764a9eab32bc1d8590efbc73d604ca25a74bce11
te-sh <admire.japan@gmail.com>
2018-04-29 13:27:07 +0900

    set actioncable url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  config.log_tags = [ :request_id ]
+  config.log_tags = [:request_id]
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
@@ -76,4 +76,5 @@ Rails.application.configure do
 
   # ActionCable setting
   config.action_cable.allowed_request_origins = ['https://worldwolf.herokuapp.com/']
+  config.action_cable.url = 'wss://worldwolf-api.herokuapp.com/cable'
 end
